df0b051e1df2d42972a539a5c92f451a4af2aaef
vagrant <pihugupta0501@gmail.com>
2024-01-10 17:05:38 +0000

    Created a function that compares two arrays through loop, and tested with assertion

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// eqArrays function 
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+}
+
+//Test Case
+//Function call
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 6]), true); // => should FAIL
