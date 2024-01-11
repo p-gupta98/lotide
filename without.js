a744ba92dad3c9b2eabde2aff46e120aed41a16e
vagrant <pihugupta0501@gmail.com>
2024-01-11 00:39:42 +0000

    Added a test case

--- without.js
+++ without.js
@@ -34,7 +34,7 @@ const without = function(sourceArray, itemsToRemoveArray) {
       newArray.push(value);
     }
   }
-  console.log(newArray);
+  return(newArray);
 };
 
 
@@ -48,3 +48,5 @@ const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // => should PASS
