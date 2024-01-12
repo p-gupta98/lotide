4f689b1a450c74221bcbed9b188db104e0acea1c
vagrant <pihugupta0501@gmail.com>
2024-01-12 02:06:26 +0000

    Created a function that takes an object and value as input. Then it loops through the object using a for in loop and compares the value input to the values in the object. It returns the key corresponding to the value in the object

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+// Function Implementation
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+//                        bestTVShowsByGenre, "The Wire"
+const findKeyByValue = function(object, value) {
+  for (const key in object) { //scifi in bestTVShowsByGenre
+    if (object[key] === value) {
+      return key;
+    }
+  }
+}
+
+//Test Case
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
