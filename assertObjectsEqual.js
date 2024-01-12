184b4f8db673196bd62902807172584500bcfff0
vagrant <pihugupta0501@gmail.com>
2024-01-12 03:52:24 +0000

    Created new assertion function for Objects

---
+++ assertObjectsEqual.js
@@ -0,0 +1,35 @@
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a false!
+const eqObjects = function(object1, object2) {
+  const arrayOne = Object.keys(object1);
+  const arrayTwo = Object.keys(object2);
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (const key of arrayOne) {
+    if (Array.isArray(arrayOne[key]) && Array.isArray(arrayTwo[key])) {
+      if (!eqArrays(arrayOne[key], arrayTwo[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false
+    }
+  }
+  return true;
+};
+
+// assertObjectsEqual defninition
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  //Call eqObjects function
+  const equalBoo = eqObjects(actual, expected);
+  if (equalBoo === true) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else if (equalBoo === false) {
+    console.log(`🔴🔴🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+  console.log(`Example label: ${inspect({ a: '1', b: 2 })}`);
+};
+
+assertObjectsEqual({ a: '1', b: 2 }, { a: '1', b: 2 }) //True
