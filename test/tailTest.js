9bae5fda255af3f442c5dddcc33c9b9327117075
vagrant <pihugupta0501@gmail.com>
2024-01-16 00:09:42 +0000

    refactored test file by converting tests to mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,36 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
+describe("#tail", () => {
+  it("returns 2 elements for array length", function(){
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    const expectedOutput = result.length;
+    assert.strictEqual((expectedOutput), 2);
+  });
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+  it("returns 'Lighthouse' for first element of array intead of 'Hello' ", function(){
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    const expectedOutput = result[0];
+    assert.strictEqual((expectedOutput), "Lighthouse");
+  });
 
-// Test Case 2: Check the returned array elements
-const resultTwo = tail(["Hello"]);
-assertEqual(resultTwo.length, 0); // ensure we get back zero elements
+  it("returns 'Labs' for second element of array intead of 'Lighthouse' ", function(){
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    const expectedOutput = result[1];
+    assert.strictEqual((expectedOutput), "Labs");
+  });
 
-//Test
-const words = ["Hello", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+  it("returns 0 elements for array length ", function(){
+    const resultTwo = tail(["Hello"]);
+    const expectedOutput = resultTwo.length;
+    assert.strictEqual((expectedOutput), 0);
+  });
+
+  it("returns 3 elements for array length - tail does not modify the original array", function(){
+    const words = ["Hello", "Lighthouse", "Labs"];
+    tail(words);
+    const expectedOutput = words.length;
+    assert.strictEqual((expectedOutput), 3);
+  });
+
+});
